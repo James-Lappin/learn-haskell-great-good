546a4c6ea8b3e5dac3b18c0fa323cc28a2eaef5d
James Lappin <lappintheyoung@gmail.com>
2019-04-01 21:35:43 +0100

    Chapter 2 - Types

--- baby.hs
+++ baby.hs
@@ -4,5 +4,6 @@ boomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <-xs, odd x]
 
 length' xs = sum [1 | _ <- xs]
 
-removeUpperCase st = [c | c <- st, c `elem` ['A'..'Z']]
+removeNonUpperCase :: [Char] -> [Char]
+removeNonUpperCase st = [c | c <- st, c `elem` ['A'..'Z']]
 
---
+++ types.hs
@@ -0,0 +1,21 @@
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
+
+factorial :: Integer -> Integer
+factorial n = product[1..n]
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+--Int is bounded to the to either 32/64 depending on your hardware
+--Integer is not bounded
+--Float is a single floating point
+--Double is a double floating point
+--Num is a Type class that consists of Int, Integer, Float and double
+--Floating is a Type class that consists of Float and Double
+--Integral is a Type class that consists of Int and Integer
+
+
+-- Probably not the best way to declare it but trying to follow along
+lengthAndAdd :: (Num a1) => [a2] -> a1 -> a1
+lengthAndAdd l a = fromIntegral(length l) + a
