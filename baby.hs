e822289d90c07c0c7a98d3350d526073dd9ac981
James Lappin <james.lappin@checkout.com>
2019-04-23 18:34:45 +0100

    Finishing chapter 4

--- baby.hs
+++ baby.hs
@@ -5,5 +5,6 @@ boomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <-xs, odd x]
 length' xs = sum [1 | _ <- xs]
 
 removeNonUpperCase :: [Char] -> [Char]
+-- list comprehension
 removeNonUpperCase st = [c | c <- st, c `elem` ['A'..'Z']]
 
