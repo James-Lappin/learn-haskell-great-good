7d25fce07acb3aefc34adfffa153fdb5de97097d
James Lappin <lappintheyoung@gmail.com>
2019-05-06 21:52:17 +0100

    Fold examples

--- Chapter 5/folds.hs
+++ Chapter 5/folds.hs
@@ -7,6 +7,33 @@ sum' xs = foldl (\acc x -> acc + x) 0 xs
 -- which is the same as
 -- magic (or as the functions are curried we will return a function that takes a list)
 sum'' :: (Num a) => [a] -> a
-sum'' = foldl + 0
+sum'' = foldl (+) 0
 
+-- Rigth fold
+mapr :: (a -> b) -> [a] -> [b]
+mapr f xs = foldr (\x acc -> f x : acc) [] xs
 
+-- 
+mapl :: (a -> b) -> [a] -> [b]
+mapl f xs = foldl (\acc x -> acc ++ [f x]) [] xs
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' y ys = foldr (\x acc -> if x == y then True else acc) False ys
+
+reverse' :: [a] -> [a]
+reverse' = foldl(\acc x -> x : acc) []
+
+reverse'' :: [a] -> [a]
+reverse'' = foldl (flip (:)) []
+
+product' :: (Num a) => [a] -> a
+product' = foldl (*) 1
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' p = foldr (\x acc -> if p x then x:acc else acc) []
+
+last' :: [a] -> a
+last' = foldl1 (\_ x -> x)
+
+and' :: [Bool] -> Bool
+and' xs = foldr (&&) True xs
