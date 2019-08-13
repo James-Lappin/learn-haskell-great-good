05677d0b250c600d8d5c601b76634e21d0880b89
James Lappin <james.lappin@checkout.com>
2019-08-13 22:11:44 +0100

    record types

---
+++ Chapter7/Record.hs
@@ -0,0 +1,8 @@
+data Person = Person { firstname :: String 
+                     , lastname :: String
+                     , age :: Int
+                     , height :: Float
+                     , phoneNumber :: String
+                     , flavor :: String } deriving (Show)
+                     
+
